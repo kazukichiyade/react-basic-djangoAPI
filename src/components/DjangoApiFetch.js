238d3c090f58b5164cba0477f5981d9072d39341
k_sakata <k_sakata@pal-style.co.jp>
2020-06-28 22:10:22 +0900

    SPA CREATE及びUPDATE機能

--- src/components/DjangoApiFetch.js
+++ src/components/DjangoApiFetch.js
@@ -5,6 +5,7 @@ const DjangoApiFetch = () => {
 
     const [tasks, setTasks] = useState([])
     const [selectedTask, setSelectedTask] = useState([])
+    const [editedTask, setEditedTask] = useState({id:'', title:''})
     const [id, setId] = useState(1)
 
     useEffect(() => {
@@ -24,12 +25,43 @@ const DjangoApiFetch = () => {
             .then(res => {setSelectedTask(res.data)})
     }
 
-    const deleteTask = () => {
+    const deleteTask = (id) => {
         axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
             headers: {
                 'Authorization': 'Token 8434f1da3263ff37d87b87c23b0910cf02731c1d'
             }})
             .then(res => {setTasks(tasks.filter(task => task.id !== id)); setSelectedTask([])})
+    }
+
+    const newTask = (task) => {
+        const data = {
+            title: task.title
+        }
+        axios.post(`http://127.0.0.1:8000/api/tasks/`, data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token 8434f1da3263ff37d87b87c23b0910cf02731c1d'
+            }})
+            .then(res => {setTasks([...tasks, res.data]); setEditedTask({id:'', title:''})}
+        )
+    }
+
+    const editTask = (task) => {
+        axios.put(`http://127.0.0.1:8000/api/tasks/${task.id}/`, task, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token 8434f1da3263ff37d87b87c23b0910cf02731c1d'
+            }})
+            .then(res => {setTasks(tasks.map(task => (task.id === editedTask.id ? res.data : task)));
+                setEditedTask({id:'',title:''})
+            
+        })
+    }
+
+    const handleInputChange = () => evt => {
+        const value = evt.target.value;
+        const name = evt.target.name;
+        setEditedTask({...editedTask, [name]:value})
     }
 
     return (
@@ -40,8 +72,10 @@ const DjangoApiFetch = () => {
                     <button onClick={() => deleteTask(task.id)}>
                         <i className="fas fa-trash-alt"></i>
                     </button>
-                    
-                    
+
+                    <button onClick={() => setEditedTask(task)}>
+                        <i className="fas fa-pen"></i>
+                    </button>
                     </li>)
                 }
             </ul>
@@ -52,6 +86,15 @@ const DjangoApiFetch = () => {
             <button type='button' onClick={() => getTask()}>Get Task</button>
             {/* <button type='button' onClick={() => deleteTask()}>DELETE</button> */}
             <h3>{selectedTask.title} {selectedTask.id}</h3>
+
+            <input type="text" name="title"
+            value = {editedTask.title}
+            onChange={handleInputChange()}
+            placeholder="New task ?" required/>
+
+            {editedTask.id ?
+            <button onClick={(()=> editTask(editedTask))}>Update</button> :
+            <button onClick={(()=> newTask(editedTask))}>Create</button> }
         </div>
     )
 }
